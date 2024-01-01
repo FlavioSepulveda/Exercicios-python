af3211754b470a2f701ac02b015595f54503a65e
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-01 19:47:09 -0300

    Pausa 1

--- EXERCICIOS/Exercicios - 1/Exercicio01.py
+++ EXERCICIOS/Exercicios - 1/Exercicio01.py
@@ -1,1 +1,22 @@
-# Exercicio 01 -Armazenamentos
+# Exercicio 01 - Armazenamentos
+
+# Declaração de variáveis do programa;
+# A, B, C, D são declaradas na atribuição de valores;
+
+# Atribuir o valor 0 a variável D
+D = 0
+# Exibir na tela o valor e uma explicação dele "O valor de x é: y" usando o comando/método print
+print(("O valor de D é: ", D))
+# Atribuir o valor 2 a variável A
+# Exibir na tela o valor e uma explicação dele "O valor de x é: y" usando o comando/método print
+
+# Atribuir o valor 3 a variável B
+# Exibir na tela o valor e uma explicação dele "O valor de x é: y" usando o comando/método print
+
+# Atribuir o valor 5 a variável C
+# Exibir na tela o valor e uma explicação dele "O valor de x é: y" usando o comando/método print
+
+# Atualize o valor de C atribuindo + 1
+# Exibir na tela o valor e uma explicação dele "O valor de x é: y" usando o comando/método print
+
+# Atribuir ao valor de D o Valor da soma de A + o valor atual de C
