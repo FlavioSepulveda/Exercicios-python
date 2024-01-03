669cbf02ed58dadf4a8b0207492bdef8deff1fd4
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-03 17:32:41 -0300

    Exercicio 2 - Multiplicação

---
+++ EXERCICIOS/Exercicios - 2/Exercicio02.2.py
@@ -0,0 +1,13 @@
+# Criando calculador de produto
+
+# entradas:
+# Pedindo os valores ao usuario
+num1 = input('Qual o primeiro valor: ')
+num1 = int(num1) # Convertendo a Numero inteiro pra evitar erros na operação
+
+num2 = input('Qual o segundo valor: ')
+num2 = int(num2) # Convertendo em Numero inteiro pra evitar erro na operação
+
+mult = num1 * num2
+
+print('O resultado da multiplicação de', num1, 'e', num2, 'é', mult)
---
+++ EXERCICIOS/Exercicios - 2/Exercicio02.py
@@ -0,0 +1,13 @@
+# Calculando o triplo de um numero fornecido pelo usuario
+
+# Solicite o valor para o usuario:
+
+numero = int(input('Insira o valor a ser triplicado: '))
+
+
+# Triplique o valor:
+def triplicaNum():
+    return numero * 3
+# Imprima a mensagem que indica que o valor x é o triplo de y
+
+print(triplicaNum())
