64c9381448094ab86951c1652d933d2169f32fc9
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 15:31:55 -0300

    Calculando percentual

---
+++ EXERCICIOS/Exercicios - 3/Exercicio03.2.py
@@ -0,0 +1,34 @@
+# Calculando variação percentual
+# Limpando a tela do sistema operacional com a importação do módulo OS.
+import os
+# Acessando comandos do sistema operacional
+os.system("cls") or None
+
+# Solicitando valor inicial
+while True:
+    valIni = input('Insira o valor inicial: ')
+    valIni = int(valIni)
+
+    if valIni > 0:
+        break
+
+print()
+# Solicitando valor final
+while True:
+    valFin = input('Insira o valor Final: ')
+    valFin = int(valFin)
+
+    if valIni >= 0:
+        break
+
+print()
+
+# Criando as variaveis:
+difVal = valFin - valIni
+Perc = (abs(difVal) / valIni) * 100
+
+# Iniciando as condicionais:
+if difVal < 0:
+    print('A variação percentual foi de -', Perc, "%")
+else:
+    print('A variação percentual foi de +', Perc, "%")
